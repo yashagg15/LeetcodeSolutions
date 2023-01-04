1c75a7dc97c180680db6c9f2d1e35b4616b6dfba
Yash Agarwal <yasagarw3@publicisgroupe.net>
2023-01-04 23:35:39 +0530

    Dikstra solution

--- Graph/cheap_flight_within_k_stop.java
+++ Graph/cheap_flight_within_k_stop.java
@@ -1,2 +1,57 @@
-package Graph;public class cheap_flight_within_k_stop {
+package Graph;
+
+// Based on dijkstra problem.
+public class cheap_flight_within_k_stop {
+    class Solution {
+        int result=Integer.MAX_VALUE;
+
+        public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+
+
+            Map<Integer,Map<Integer,Integer>> mp=new HashMap<>();
+            for(int i=0;i<n;i++){
+                mp.put(i,new HashMap<>());
+            }
+
+            for(int i=0;i<flights.length;i++){
+                int s=flights[i][0];
+                int d=flights[i][1];
+                int val=flights[i][2];
+                mp.get(s).put(d,val);
+            }
+            int dist[]=new int[n];
+            for(int i=0;i<n;i++)
+                dist[i]=Integer.MAX_VALUE;
+            int count=0;
+            Queue<int[]> queue=new LinkedList<>();
+            queue.add(new int[]{0,src});
+
+            while(!queue.isEmpty() && count<=k){
+                int siz=queue.size();
+                while(siz>0){
+
+                    int[] temp=queue.poll();
+                    int point=temp[1];
+                    int curDist=temp[0];
+                    Map<Integer,Integer> m=mp.get(point);
+                    for(Map.Entry<Integer,Integer> p:m.entrySet()){
+                        int des=p.getKey();
+                        if(dist[des]>p.getValue()+curDist){
+                            dist[des]=p.getValue()+curDist;
+                            queue.add(new int[]{p.getValue()+curDist,des});
+                        }
+                        else
+                            continue;
+
+
+                    }
+                    siz--;
+                }
+
+                count++;
+            }
+            return dist[dst]==Integer.MAX_VALUE?-1:dist[dst];
+
+        }
+    }
 }
--- Graph/path_with_maximum_probability.java
+++ Graph/path_with_maximum_probability.java
@@ -1,2 +1,67 @@
-package Graph;public class path_with_maximum_probability {
+package Graph;
+
+//Based on Dijkstra
+public class path_with_maximum_probability {
+
+    class Solution {
+
+        class Prob{
+            public int key;
+            public double value;
+
+            public Prob(int key,double value){
+                this.key=key;
+                this.value=value;
+            }
+            public int getKey(){
+                return key;
+            }
+            public double getValue(){
+                return value;
+            }
+        }
+
+        public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+
+            Map<Integer,Map<Integer,Double>> mp=new HashMap<>();
+
+            for(int i=0;i<n;i++)
+                mp.put(i,new HashMap<>());
+
+            for(int i=0;i<edges.length;i++){
+                mp.get(edges[i][0]).put(edges[i][1],succProb[i]);
+                mp.get(edges[i][1]).put(edges[i][0],succProb[i]);
+            }
+            Prob prob=new Prob(start,1);
+
+            PriorityQueue<Prob> queue=new PriorityQueue<>((a,b)->{
+                if(b.getValue()>a.getValue())
+                    return 1;
+                return -1;
+            });
+            queue.add(prob);
+            double succ[]=new double[n];
+            succ[start]=1;
+
+            while(!queue.isEmpty()){
+                Prob p=queue.poll();
+                int key=p.getKey();
+                double value=p.getValue();
+                Map<Integer,Double> temp=mp.get(key);
+                for(Map.Entry<Integer,Double> m:temp.entrySet()){
+                    int upcomingKey=m.getKey();
+                    double probValue=m.getValue();
+                    double mul=value*probValue;
+                    if(mul>succ[upcomingKey]){
+                        succ[upcomingKey]=mul;
+                        queue.add(new Prob(upcomingKey,mul));
+                    }
+
+                }
+            }
+
+            return succ[end];
+
+        }
+    }
 }
